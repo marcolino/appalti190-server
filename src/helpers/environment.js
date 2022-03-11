1295910fca85d0874712ce7e891f0fe85d93be4b
marco <m.solari@mypass.technology>
2022-03-11 19:48:04 +0100

    deploying to heroku

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,4 +1,5 @@
 const config = require("../config");
+const sendemail = require("../helpers/sendemail");
 
 const assertEnvironment = () => {
   console.log("assertEnvironment");
--- src/helpers/sendemail.js
+++ src/helpers/sendemail.js
@@ -1,12 +1,33 @@
 const sgMail = require("@sendgrid/mail");
+const config = require("../config");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-module.exports = (mailOptions) => {
+const sendemail = (mailOptions) => {
+  //console.log("sgMail.send options:", mailOptions);
   return new Promise((resolve, reject) => {
     sgMail.send(mailOptions, (error, result) => {
+      //console.log("sgMail.send response:", error, result);
       if (error) return reject(error);
       return resolve(result);
     });
   });
+};
+
+module.exports = {
+  sendemail: async (mailOptions) => {
+    sendemail(mailOptions);
+  },
+  assertionsCheckFailure: async (body) => {
+    const subject = `${config.realm} ~ Controllo asserzione fallito`;
+    const to = config.emailAdministration.to;
+    const from = config.emailAdministration.from;
+    const html = body;
+    console.log(`sending email to: ${to} from: ${from}, subject: ${subject}, body: ${html}`);
+    try {
+      await sendemail({to, from, subject, html});
+    } catch(error) {
+      console.error("Error sending email:", error);
+    }
+  },
 }
