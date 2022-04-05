8413236dac6958a659ccc87326c60ffb5d5bc037
marco <m.solari@mypass.technology>
2022-04-05 15:39:28 +0200

    deploying to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const logger = require("morgan");
+const logger = require("winston");
 const db = require("./src/models");
 const { assertEnvironment } = require("./src/helpers/environment");
 const config = require("./src/config");
