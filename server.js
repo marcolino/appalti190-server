4821a800627c0c22ef297047823366ffeb83af52
marcolino <marcosolari@gmail.com>
2022-06-21 18:10:49 +0200

    deploying to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { logger, colors } = require("./src/controllers/logger.controller");
 const db = require("./src/models");
 const { assertEnvironment } = require("./src/helpers/environment");
-const { sendemail } = require(".src/helpers/notification");
+const { sendemail } = require("./src/helpers/notification");
 const config = require("./src/config");
 
 const production = (process.env.NODE_ENV === "production");
