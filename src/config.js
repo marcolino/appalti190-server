355dab4a714634ecdb3f43987a59e7e0ded5a19a
marcolino <marcosolari@gmail.com>
2022-06-23 09:45:33 +0200

    Using @arsistemi.it FROM email to avoid gmail to mark our emails as SPAM

--- src/config.js
+++ src/config.js
@@ -136,7 +136,7 @@ module.exports = {
   },
   emailAdministration: {
     //from: "marcosolari@gmail.com",
-    from: "noreply@arsistemi.it", // TODO: check if this works and avoid SPAM marks by gmail...
+    from: "antonio.rossi@arsistemi.it", // TODO: check if this works and avoid SPAM marks by gmail...
     to: "marcosolari@gmail.com",
   },
   envRequiredVariables: [ // TODO: check all of these are necessary
